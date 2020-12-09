d02c87cae59cf25668b0b11f8bc16230c99a2865
Mike Owino <mike.owino40@yahoo.com>
2020-12-09 12:43:08 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const getData = async () => {
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint', {
+      method: 'POST',
+      body: JSON.stringify({id: 200})
+    })
+    if(response.ok){
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch(error) {
+    console.log(error);
+  }
+}
